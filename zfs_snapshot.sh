7aaca80207d5a9efa6b2d5f1245a704f5b2f05e9
nik <vasileiadis@strato.de>
2017-04-27 20:11:36 +0200

    Support Linux

--- zfs_snapshot.sh
+++ zfs_snapshot.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
-# This script is tested only on FreeBSD and is most probably not portable. Patches
-# are welcome :)
-# It should run from cron every few hours.
-# It creates a snapshot when it runs and deletes the snapshot that was created exactly
-# a week ago. Although it is just a sample it could be of help.
+# This script is tested only on FreeBSD and GNU/Linux. It won't work
+# on other systems. Patches are welcome :)
+#
+# It should run from cron every few hours. It creates a recursive snapshot
+# and deletes the snapshot that was created exactly two weeks ago. Although
+# it is just a sample, it could be of help.
 # Usage:
 # sh zfs_snapshot.sh pool_name
 # or
 # sh zfs_snapshot.sh pool_name/root/important_filesystem
 
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-t=$(date -j '+%Y%m%d%H%M' `date '+%Y%m%d%H%M'`)
-
-snap=`date -j '+%Y-%m-%d_%H-%M' $t `-`hostname -s`
-oldsnap=`date -j -v -1w '+%Y-%m-%d_%H-%M' $t `-`hostname -s`
+# Subtracting dates is not portable.
+if [ `uname -s` = FreeBSD ]
+then
+	jflag=-j
+	now=`date '+%Y%m%d%H%M'`
+	oldsnap=`date $jflag -v -2w "$now" '+%Y-%m-%d_%H-%M' `-`hostname -s` 
+elif [ `uname -s` = Linux ]
+then
+	jflag=-d
+	now=`date '+%Y%m%d %H%M'`
+	oldsnap=`date $jflag "$now - 2 weeks" '+%Y-%m-%d_%H-%M' `-`hostname -s` 
+fi
+# This works on both FreeBSD and GNU/Linux.
+snap=`date $jflag "$now" '+%Y-%m-%d_%H-%M' `-`hostname -s`
 
 for fs in $*
 do
-	zfs snapshot -r ${fs}@${snap}
-
-	if ! echo $snap | grep '^20..-..-.._06-..' >/dev/null
-	then
-		zfs destroy -r ${fs}@${oldsnap} 2>&1 |
-		    sed '/could not find any snapshots to destroy; check snapshot names./d'
-	fi
+	# When something fails, print a warning and continue
+	zfs snapshot -r ${fs}@${snap} || echo exitcode: $?
+	zfs destroy -r ${fs}@${oldsnap} || echo exitcode: $?
 done
